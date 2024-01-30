6b570212ee137a26f031893c3d5fd2deffcb6b06
Youtong0826 <jimmy2007826@gmail.com>
2024-01-30 22:40:35 +0800

    Nested Ranges Check

---
+++ Sorting and Searching/Nested Ranges Check.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define pii pair<int,int>
+#define f first
+#define s second
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie()
+using namespace std;
+
+
+// 1 6
+// 2 4
+// 4 8
+// 3 6
+
+// 1 2 3 4
+// 4 6 6 8
+
+signed main(){
+    int n;
+    cin >> n;
+
+}
