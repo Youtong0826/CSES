606f40b8d990a140e2788042e30c952efb8b6102
Youtong0826 <jimmy2007826@gmail.com>
2024-02-04 00:09:22 +0800

    Factory Machines

--- Sorting and Searching/Factory Machines.cpp
+++ Sorting and Searching/Factory Machines.cpp
@@ -21,7 +21,7 @@ signed main(){
     int l = 0, r = INT64_MAX, ans = INT64_MAX;
     while (r - l != 1){
         int mid = (r+l)/2;
-        cout << l << ' ' << r << ' ' << mid << '\n';
+        //cout << l << ' ' << r << ' ' << mid << '\n';
 
         int cnt = 0, fl = 0;
         for (int i = 0; i < n; i++){
