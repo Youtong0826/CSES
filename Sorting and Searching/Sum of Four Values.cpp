6e41216d0e9655d847aa3ac408e977fb21c65384
Youtong0826 <jimmy2007826@gmail.com>
2024-02-14 23:31:27 +0800

    fixed

--- Sorting and Searching/Sum of Four Values.cpp
+++ Sorting and Searching/Sum of Four Values.cpp
@@ -1,50 +1,44 @@
 #include <bits/stdc++.h>
 #define int long long
+#define tii tuple<int, int, int>
 #define pii pair<int, int>
 #define f first
 #define s second
-#define fastio                    \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0)
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0)
 using namespace std;
-// declare
+
 int n, x;
-vector<int> v;
-set<pair<int, pii>> Lsum, Rsum;
-//
-signed main()
-{
+vector<pii> v;
+vector<tii> cnt;
+set<tii> st;
+
+signed main(){
     fastio;
     cin >> n >> x;
-    for (int i = 0; i < n; i++)
-    {
-        int x;
-        cin >> x;
-        v.push_back(x);
+
+    v.resize(n+1);
+    for (int i = 1; i <= n; i++){
+        cin >> v[i].f;
+        v[i].s = i;
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-        {
-            Rsum.insert({v[i] + v[j], {i, j}});
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            if (i != j){
+                st.insert(make_tuple(v[i].f + v[j].f, i, j));
+                cnt.push_back(make_tuple(v[i].f + v[j].f, i, j));
+            }
         }
     }
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j < n; j++)
-            Rsum.erase({v[i] + v[j], {i, j}});
-            
-        for (int j = 0; j < i; j++)
-        {
-            int k = x - (v[i] + v[j]);
-            pair<int, pii> p = {k, {-1, -1}};
-            auto it = Rsum.lower_bound(p);
-            if (it == Rsum.end() || it->f != k)
-                continue;
-            cout << i + 1 << " " << j + 1 << " " << it->s.f + 1 << " " << it->s.s + 1 << "\n";
+
+    for (auto i: cnt){
+        int t = x - get<0>(i);
+        auto it = st.lower_bound(make_tuple(t, -1, -1));
+        if (it != st.end() && get<0>(*it) == t && get<1>(*it) != get<1>(i) && get<2>(*it) != get<2>(i) && get<1>(i) != get<2>(*it) && get<2>(i) != get<1>(*it)){
+            cout << get<1>(i) << ' ' << get<2>(i) << ' ' << get<1>(*it) << ' ' << get<2>(*it) << '\n';
             return 0;
         }
     }
-    cout << "IMPOSSIBLE\n";
-    return 0;
+
+    cout << "IMPOSSIBLE";
 }
