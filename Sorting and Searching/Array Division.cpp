642b14913ba8a126caea1bb3911ed3e05a01d68b
Youtong0826 <jimmy2007826@gmail.com>
2024-02-20 04:43:28 +0800

    Array Division

--- Sorting and Searching/Array Division.cpp
+++ Sorting and Searching/Array Division.cpp
@@ -1,11 +1,50 @@
 #include <bits/stdc++.h>
+#define pii pair<int,int>
+#define f first
+#define s second
+#define int long long
+#define Youtong ios::sync_with_stdio(0); cin.tie(0); cout.tie()
 using namespace std;
 
-int main(){
-    int n, nums[200010];
-    for (int i = 0; i < n; i++)
-        cin >> nums[i];
+int n, k;
+vector<int> v;
 
-    sort(nums, nums+n);
+bool good(int t, int k){
+    int sum = 0, sa = 0;
+
+    for (auto x: v){
+        if (x > t)
+            return 0;
+
+        if (sum + x > t){
+            sum = 0;
+            sa++;
+        }
+        sum += x;
+    }
+
+    if (sum > 0)
+        sa++;
+
+    return sa <= k;
+}
+
+signed main(){
+    Youtong;
+    cin >> n >> k;
+    v.resize(n);
+    for (auto &x: v)
+        cin >> x;
     
+    int l = 0, r = INT64_MAX;
+    while (l < r){
+        int mid = l + (r-l)/2;
+        //cout << l << ' ' << r << '\n';
+        if (good(mid, k))
+            r = mid;
+        else
+            l = mid+1;
+    }
+    
+    cout << r << '\n';
 }
