bcb0a123b8bcd6b95d6e9b0e180f537d0709af74
Youtong0826 <jimmy2007826@gmail.com>
2024-04-29 22:45:54 +0800

    Collectiong Numbers ||

--- Sorting and Searching/Collecting Numbers II.cpp
+++ Sorting and Searching/Collecting Numbers II.cpp
@@ -1,36 +1,38 @@
 // 待解
-
+// 以下是參考 Coding-Prep 的做法
 #include <bits/stdc++.h>
 using namespace std;
+int main(){    
+    int n, q;
+    cin >> n >> q;
+    vector <int> a(n+1), idx(n+2);
+    idx[0] = 0, idx[n+1] = n+1; // 方便實作
+    for(int i = 1;i <= n;i++){
+        cin >> a[i];
+        idx[a[i]] = i;
+    }
+    int cnt = 1;
+    for(int i = 1;i <= n;i++){
+        if(idx[i] <= idx[i-1])
+            cnt++;
+    }
 
-const int maxn = 2e5+5;
-int arr[maxn], idx[maxn];
+    while(q--){
+        int l, r; 
+        cin >> l >> r;
+        int x = a[l], y = a[r];
+        swap(a[l], a[r]); // 判斷交換時候要怎麼改變答案
+        if (idx[x-1] <= idx[x] && idx[x-1] > r) cnt++;
+        if (idx[x-1] > idx[x] && idx[x-1] <= r) cnt--;
+        if (idx[x] <= idx[x+1] && r > idx[x+1]) cnt++;
+        if (idx[x] > idx[x+1] && r <= idx[x+1]) cnt--;		
+        idx[x] = r;
+        if (idx[y-1] <= idx[y] && idx[y-1] > l) cnt++;
+        if (idx[y-1] > idx[y] && idx[y-1] <= l) cnt--;
+        if (idx[y] <= idx[y+1] && l > idx[y+1]) cnt++;
+        if (idx[y] > idx[y+1] && l <= idx[y+1]) cnt--;	
+        idx[y] = l;
 
-void swap(int x, int y){
-    int temp = arr[x];
-    arr[x] = arr[y];
-    arr[y] = temp;
-}
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int n, m, x, y;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        cin >> arr[i];
-
-    while (m--){
-        cin >> x >> y;     
-        swap(x, y);
-        for (int i = 1; i <= n; i++)
-            idx[arr[i]] = i;
-
-        int ans = 1;
-        for (int i = 1; i < n; i++)
-            if (idx[i] > idx[i+1])
-                ans++;
-
-        cout << ans << '\n';
+        cout << cnt << '\n';
     }
 }
