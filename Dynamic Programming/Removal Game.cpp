b2d3340b0d54b9b34326d70dfda940e69847567c
Youtong0826 <jimmy2007826@gmail.com>
2024-06-06 20:02:41 +0800

    Removal Game

--- Dynamic Programming/Removal Game.cpp
+++ Dynamic Programming/Removal Game.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-// dp[l][r] = [l, r] 先手的最佳策略
-// dp[l][r] = max(a[l, r] - a[l]+dp[l+1, r], a[l, r]-a[r]+dp[l, r-1]);
+// dp[l][r] = 區間 [l, r] 最佳策略
+// dp[l][r] = max(sum(l, r) - dp[l+1][r], sum(l, r)-dp[l, r-1]);
 
 const int maxn = 5e3+1;
 int arr[maxn], pre[maxn], dp[maxn][maxn];
 
-int main() {
+signed main() {
     int n;
     cin >> n;
 
     for (int i = 1; i <= n; i++) {
         cin >> arr[i];
         pre[i] = pre[i-1]+arr[i];
+        dp[i][i] = arr[i];
     }
 
-    for (int)
+    for (int len = 2; len <= n; len++) {
+        for (int i = 1; i+len-1 <= n; i++) {
+            int j = i+len-1, p = pre[j]-pre[i-1];
+            dp[i][j] = max(p - dp[i+1][j], p - dp[i][j-1]);
+            // cout << i << ' ' << j << ' ' << p << ' ' << dp[i][j] << '\n';
+        }
+    }
+
+    cout << dp[1][n] << '\n';
 }
