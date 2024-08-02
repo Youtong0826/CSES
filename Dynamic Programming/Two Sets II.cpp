e00f9fa8f6512f601443e3f18b1f3823f381b4aa
Youtong0826 <jimmy2007826@gmail.com>
2024-08-02 18:12:06 +0800

    Two Set II

---
+++ Dynamic Programming/Two Sets II.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// dp[i] 為 i 可以分為幾種組合
+
+// dp[1] = 1
+// dp[2] = 0
+// dp[3] = 1
+// dp[4] = 1
+// dp[5] = 0
+// dp[6] = 0
+// dp[7] = 4 
+int main(){
+    int n;
+    cin >> n;
+}
