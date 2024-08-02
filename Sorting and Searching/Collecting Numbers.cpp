3cb3d49d95cf7941da77509eba0f45789d3e460f
Youtong0826 <jimmy2007826@gmail.com>
2024-08-02 18:14:42 +0800

    Collection Numbers

--- Sorting and Searching/Collecting Numbers.cpp
+++ Sorting and Searching/Collecting Numbers.cpp
@@ -1,20 +1,41 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int idx[200005];
+
+// int main(){
+//     int n, a;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++){
+//         cin >> a;
+//         idx[a] = i;
+//     }
+
+//     int ans = 1;
+//     for (int i = 2; i <= n; i++)
+//         if (idx[i] < idx[i-1])
+//             ans++;
+
+//     cout << ans;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int idx[200005];
+int arr[200005];
 
 int main(){
-    int n, a;
+    int n, ans = 1, x;
     cin >> n;
-    for (int i = 1; i <= n; i++){
-        cin >> a;
-        idx[a] = i;
+    for (int i = 0; i < n; ++i){
+        cin >> arr[i];
     }
-
-    int ans = 1;
-    for (int i = 2; i <= n; i++)
-        if (idx[i] < idx[i-1])
+    
+    for (int i = 1; i <= n; ++i){ 
+        if (arr[i] < arr[i - 1]){
             ans++;
-
+        }
+    }
+    
     cout << ans;
 }
