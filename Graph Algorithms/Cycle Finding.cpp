7b4637e29c895640297ac5bea50f704c53eb5807
Youtong0826 <jimmy2007826@gmail.com>
2024-08-03 02:50:49 +0800

    Cycle Finding

--- Graph Algorithms/Cycle Finding.cpp
+++ Graph Algorithms/Cycle Finding.cpp
@@ -1,30 +1,55 @@
 #include <bits/stdc++.h>
+#define int long long
 #define pii pair<int, int>
 using namespace std;
 
-vector<int> dis;
+const int maxn = 2e5+1;
+bool vis[maxn];
+vector<int> dis, from, ans;
 vector<array<int, 3>> edges;
 
-int main() {
-    int n, m, a, b, c;
+signed main() {
+    int n, m, a, b, c, last = -1;
     cin >> n >> m;
-    dis.assign(n+1, 1e18);
+    dis.assign(n+5, 1e18);
+    from.resize(n+5);
     for (int i = 0; i < m; i++) {
         cin >> a >> b >> c;
         edges.push_back({a, b, c});
     }
 
     dis[1] = 0;
-    bool f = 0;
     for (int i = 1; i <= n; i++) {
         for (auto [a, b, c]: edges){
             if (dis[b] > dis[a]+c){
                 dis[b] = dis[a]+c;
+                from[b] = a;
                 if (i == n) {
-                    f = 1;
+                    last = a;
                 }
             }
         }
     }
+
+    if (last == -1){
+        cout << "NO";
+        return 0; 
+    }
     
+    int tmp = last;
+    ans.push_back(tmp);
+    while (!vis[last]) {
+        vis[last] = 1;
+        last = from[last];
+        ans.push_back(last);
+    }
+
+    reverse(ans.begin(), ans.end());
+    int f = 0;
+    cout << "YES\n";
+    for (auto x: ans) {
+        if (f == 2) break;
+        if (x == last) f++;
+        cout << x << ' ';
+    }
 }
